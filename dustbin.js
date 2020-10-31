9d605397b83fc76a18427205b20fdb59dda4aea0
Malkiel645 <71563100+Malkiel645@users.noreply.github.com>
2020-10-31 08:58:08 -0500

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,61 @@
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=100;
+		this.wallThickness=20;
+		this.angle=0;	
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		Matter.Body.setAngle(this.leftWallBody, this.angle);
+        
+        this.image = loadImage("dustbin.png");
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight + 20, {isStatic:true})
+		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y-50);
+			rectMode(CENTER)
+
+			angleMode(RADIANS)
+
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y-50);
+			rectMode(CENTER)
+
+			angleMode(RADIANS)
+
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y - 95);
+			rectMode(CENTER)
+
+			angleMode(RADIANS)
+
+            imageMode(CENTER);
+            image(this.image,0,0,this.dustbinWidth +10, 210)
+			pop()
+			
+	}
+
+}
